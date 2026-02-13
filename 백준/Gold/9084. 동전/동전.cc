5f00dc9beebdbd2588fb7e0dc216519f166b562b
taehyung5271 <84510789+taehyung5271@users.noreply.github.com>
2026-02-13 09:07:41 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n;
+        vector<int> coin(n);
+        for(int i=0; i<n;i++){
+            cin >> coin[i];
+        }
+        cin >> m;
+
+        vector<int> dp(m+1,0);
+        dp[0] = 1;
+        for(int i=0; i<n;i++){
+            for(int j=coin[i]; j <= m; j++){
+                dp[j] = dp[j] + dp[j - coin[i]];
+            }
+        }
+        cout << dp[m] << "\n";
+    }
+    
+    return 0;
+}
