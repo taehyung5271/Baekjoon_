d7c8ba3b0eae7cb7dcd3c6e0cc796c72fc9e063a
taehyung5271 <84510789+taehyung5271@users.noreply.github.com>
2026-03-19 15:36:56 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\\    /\\").append('\n');
+        sb.append(" )  ( ')").append('\n');
+        sb.append("(  /  )").append('\n');
+        sb.append(" \\(__)|").append('\n');
+        System.out.println(sb);
+    }
+}
