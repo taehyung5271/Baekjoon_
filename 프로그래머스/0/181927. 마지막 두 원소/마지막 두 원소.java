710c5ccf3f17860f4807c1fe7276033f59f1fa71
taehyung5271 <84510789+taehyung5271@users.noreply.github.com>
2026-03-31 09:18:36 +0900

    [level 0] Title: 마지막 두 원소, Time: 2.98 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,15 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> answer = new ArrayList<>();
-        for(int i=0; i<num_list.length;i++){
-            answer.add(num_list[i]);
-        }
-        if(num_list[num_list.length-1] > num_list[num_list.length-2]){
-            answer.add(num_list[num_list.length-1] - num_list[num_list.length-2]);
-        }else{
-            answer.add(num_list[num_list.length-1]*2);
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = {};
+        return answer;
     }
 }
