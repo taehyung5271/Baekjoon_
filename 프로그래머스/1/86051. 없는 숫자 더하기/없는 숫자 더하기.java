ab42cac64616b7b345c59a6ff5b5d1ec5923316b
taehyung5271 <84510789+taehyung5271@users.noreply.github.com>
2026-05-06 10:59:19 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.08 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<numbers.length;i++){
+            set.add(numbers[i]);
+        }
+        for(int i=1; i<=9; i++){
+            if(!set.contains(i)){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
