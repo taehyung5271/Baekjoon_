18fecce06eaa18420490d2decb35b3f4ee5b07d3
taehyung5271 <84510789+taehyung5271@users.noreply.github.com>
2026-05-06 10:17:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int n = arr.length;
+        for(int i=0; i < n; i++) {
+            answer += arr[i];
+        }
+        answer /= n;
+        return answer;
+    }
+}
